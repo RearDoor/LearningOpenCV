e6617edd0c359964d4dc716252a4733b91d99013
Koziah Sylvester <skoziah@gmail.com>
2024-02-09 23:59:51 +0000

    video inference success

--- main.py
+++ main.py
@@ -2,26 +2,92 @@
 import numpy as np;
 import cv2; 
 
+# Video Inference
+fourcc = cv2.VideoWriter_fourcc(*'mp4v') 
+
+vid_cap = cv2.VideoCapture('video2.mp4')
+
+frame_width = int(vid_cap.get(3))
+frame_height = int(vid_cap.get(4))
+fps = vid_cap.get(cv2.CAP_PROP_FPS)
+vid_write = cv2.VideoWriter('output2.mp4', fourcc, fps, (frame_width, frame_height))
+
+
 all_rows = open('synset.txt').read().strip().split("\n")
 classes = [r[r.find(' ') + 1:] for r in all_rows] # WHYY?
 
 net = cv2.dnn.readNetFromCaffe('bvlc_googlenet.proto.txt','bvlc_googlenet.caffemodel')
 
+if vid_cap.isOpened() == False:
+    print('Failed to open video')
 
-## Images
-img = cv2.imread('output.jpeg')
+while True:
+    ret, frame = vid_cap.read()
 
-blob = cv2.dnn.blobFromImage(img,1,(224,224))
-net.setInput(blob)
-out = net.forward()
+    if ret:
+        cv2.imwrite('output.jpeg',frame)
+        blob = cv2.dnn.blobFromImage(frame,1,(224,224))
 
-index = np.argsort(out[0])[::-1][:5]
-print (index)
-for (i,v) in enumerate(index):
-    print(i+1,classes[v],v,out[0][v])
+        net.setInput(blob)
 
-print(img.shape)
-print(type(img))
+        out = net.forward()
+
+        index = "{} {}"
+
+        row = 1
+        for i in np.argsort(out[0])[::-1][:5]:
+            txt =  index.format(classes[i], out[0][i]*100)
+            cv2.putText(frame, txt, (0,25 + 40*row), cv2.FONT_HERSHEY_PLAIN,5,(255,255,255),2)
+            row = row + 1
+
+        
+        vid_write.write(frame)
+
+    else:
+        break
+
+vid_cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## FIRST INFERENCE
+
+
+# all_rows = open('synset.txt').read().strip().split("\n")
+# classes = [r[r.find(' ') + 1:] for r in all_rows] # WHYY?
+
+# net = cv2.dnn.readNetFromCaffe('bvlc_googlenet.proto.txt','bvlc_googlenet.caffemodel')
+
+
+# ## Images
+# img = cv2.imread('output.jpeg')
+
+# blob = cv2.dnn.blobFromImage(img,1,(224,224))
+# net.setInput(blob)
+# out = net.forward()
+
+# index = np.argsort(out[0])[::-1][:5]
+# print (index)
+# for (i,v) in enumerate(index):
+#     print(i+1,classes[v],v,out[0][v])
+
+# print(img.shape)
+# print(type(img))
 
 # b = img[:,:,0]
 # g = img[:,:,1]
